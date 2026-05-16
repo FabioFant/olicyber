54393ff19e78aae0d68c5fa5962bf4eb9bd04603
FabioFant <fabiofanta07@gmail.com>
2026-05-16 08:58:11 +0200

    Fixed bugged solve.

--- crypto/1337_XOR.py
+++ crypto/1337_XOR.py
@@ -7,7 +7,6 @@ def xor(a, b):
 FLAG = bytes.fromhex("27893459dc8772d66261ff8633ba1e5097c10fba257293872fd2664690e975d2015fc4fd3c") # 37
 car = 0
 while car <= 255:
-    key = b'A\xe5U>\xa7' + os.urandom(1) # xor(FLAG[:5], b'flag{') + os.urandom(1)
+    key = b'A\xe5U>\xa7' + bytes([car])
     print(xor(FLAG, key * (len(FLAG)//len(key) + 1)))
     car += 1
-    
